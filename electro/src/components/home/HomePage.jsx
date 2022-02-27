93fa998eef86230a114dca118752f62b04250207
khurshidaalam <setukhurshidaalom@gmail.com>
2022-02-28 00:37:20 +0600

    login form

--- electro/src/components/home/HomePage.jsx
+++ electro/src/components/home/HomePage.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import "./home.css";
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
 
 const HomePage = () => {
+
+    
+
     return (
         <>
             <div className='home-login-div'>
@@ -15,8 +20,15 @@ const HomePage = () => {
                         </div>
                         <div className='col-md-6 col-12'>
                         <div className='form-div '>
-                        <form>
-                        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                        <form className='login-form'>
+                        <TextField className='login-data-input' id="outlined-basic" label="username" variant="outlined" onChange={HandleChange()}/>
+                        <TextField className='login-data-input' id="outlined-basic" label="password" variant="outlined" onChange={HandleChange()}/>
+                        <div className='login-btn'>
+                            <Button variant="contained" >login</Button>
+                        </div>
+                        <div className='to-register-div mt-3'>
+                            <p>if you don't have account, <Link to="/">Register now</Link> </p>
+                        </div>
                         </form>
                         </div>
                         

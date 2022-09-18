50008e3a53d87d20b3c030c97dc743053640602f
khurshidaalam <setukhurshidaalom@gmail.com>
2022-09-19 00:45:52 +0600

    committing 19th sept

--- electro/src/components/index.js
+++ electro/src/components/index.js
@@ -5,3 +5,47 @@ export {default as Login} from "./home/Login";
 export {default as Layout} from "./layout/Layout";
 export {default as CarrierDashboard} from "./carrierprofile/CarrierDashboard";
 export {default as Notification} from "./layout/Notification";
+
+
+
+// users={
+//     personal
+//     ..................
+//         id
+//     name:"",
+//     address:
+//     nid:
+//     email:
+//     phone:
+//     date of birth
+//     gender
+//     present address
+//     joining
+//     .................
+
+//     professional
+//     ...............
+//     status
+//     available to deliver:
+//     active at
+//     available timing
+//     call from
+//     confirm status
+//     total earnings
+//     total delivered
+//     total received
+
+//     total orders
+//     ....................
+//     index
+//     from 
+//     to
+//     customer info
+//     payment
+//     paid
+//     status
+
+    
+
+
+// }
